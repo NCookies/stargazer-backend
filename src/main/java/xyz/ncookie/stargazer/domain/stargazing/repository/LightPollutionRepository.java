63c48aa3c95f393c1893a8eb1048546a124af780
SeungWoo Ryu <swstar21c@gmail.com>
2026-01-04 17:37:31 +0900

    feat: 광해 데이터 조회 쿼리 최적화
    - 캐시를 적용하여 응답 시간을 단축시켰지만 2s 정도로 여전히 느림
    - 변경 전 쿼리
      - 모든 데이터(수십만 개)에 대해 거리(ST_Distance_Sphere)를 계산하고, 그 다음에 정렬하여 하나를 뽑는 방식임
      - 때문에 인덱스를 타지 않아 O(N)이 걸림
    - 변경 후 쿼리
      - 주변 반경 약 10km 데이터로 범위를 먼저 줄이고, 그 안에서 계산함
      - R-Tree 인덱스를 타고 후보군을 줄인 뒤 정렬하므로 속도가 대폭 개선됨
      - 쿼리 실행 시간 593ms -> 18ms

--- src/main/java/xyz/ncookie/stargazer/domain/stargazing/repository/LightPollutionRepository.java
+++ src/main/java/xyz/ncookie/stargazer/domain/stargazing/repository/LightPollutionRepository.java
@@ -12,10 +12,18 @@ import xyz.ncookie.stargazer.domain.stargazing.entity.LightPollution;
 @Repository
 public interface LightPollutionRepository extends JpaRepository<LightPollution, Long> {
 
+	/**
+	 * 1. WHERE MBRContains(...) : 인덱스를 사용해 주변(약 11km) 반경의 데이터만 1차로 필터링
+	 * 2. ORDER BY ... : 걸러진 소수의 데이터 중에서만 정밀 거리 계산 수행
+	 */
 	@Query(value = """
-		SELECT * FROM light_pollution lp
-		ORDER BY ST_Distance_Sphere(lp.location, ST_GeomFromText(:point, 4326)) ASC
+        SELECT * FROM light_pollution lp
+        WHERE MBRContains(
+            ST_Buffer(ST_GeomFromText(:point, 4326), 10000), -- 0.1도 반경 (약 10km) 버퍼 생성
+            lp.location
+        )
+        ORDER BY ST_Distance_Sphere(lp.location, ST_GeomFromText(:point, 4326)) ASC
         LIMIT 1
-	""", nativeQuery = true)
+    """, nativeQuery = true)
 	Optional<LightPollution> findNearest(@Param("point") String pointText);
 }
