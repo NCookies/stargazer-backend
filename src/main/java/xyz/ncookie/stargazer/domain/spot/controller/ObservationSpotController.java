e94d11881cba66d36ea6e5e19479cd1efce88633
SeungWoo Ryu <swstar21c@gmail.com>
2026-01-11 02:09:24 +0900

    feature: 특정 위치 기준 반경 N km 명소 찾기 기능 구현
    - 특정 좌표 반경 N km의 명소들을 조회하는 기능을 구현함
    - ObservationSpot의 위도, 경도 값을 POINT 자료형으로 바꾸는 것은 좀 더 생각해보기로 함
      - 현재 데이터 개수로는 성능상 문제가 없기 때문

--- src/main/java/xyz/ncookie/stargazer/domain/spot/controller/ObservationSpotController.java
+++ src/main/java/xyz/ncookie/stargazer/domain/spot/controller/ObservationSpotController.java
@@ -3,10 +3,13 @@ package xyz.ncookie.stargazer.domain.spot.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import xyz.ncookie.stargazer.domain.spot.dto.request.ObservationSpotRequest;
 import xyz.ncookie.stargazer.domain.spot.dto.response.ObservationSpotResponse;
 import xyz.ncookie.stargazer.domain.spot.service.ObservationSpotService;
 
@@ -18,9 +21,11 @@ public class ObservationSpotController {
 	private final ObservationSpotService observationSpotService;
 
 	@GetMapping
-	public List<ObservationSpotResponse> getObservationSpots() {
+	public List<ObservationSpotResponse> getObservationSpots(
+		@Valid @ModelAttribute ObservationSpotRequest request
+	) {
 
-		return observationSpotService.getObservationSpots();
+		return observationSpotService.getObservationSpots(request);
 	}
 
 }
--- src/main/java/xyz/ncookie/stargazer/domain/spot/service/ObservationSpotService.java
+++ src/main/java/xyz/ncookie/stargazer/domain/spot/service/ObservationSpotService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import xyz.ncookie.stargazer.domain.spot.dto.request.ObservationSpotRequest;
 import xyz.ncookie.stargazer.domain.spot.dto.response.ObservationSpotResponse;
 import xyz.ncookie.stargazer.domain.spot.repository.ObservationRepository;
 
@@ -16,10 +17,22 @@ public class ObservationSpotService {
 
 	private final ObservationRepository observationRepository;
 
-	public List<ObservationSpotResponse> getObservationSpots() {
+	public List<ObservationSpotResponse> getObservationSpots(ObservationSpotRequest request) {
+
+		if (request.lat() != null && request.lon() != null) {
+
+			double radiusKm = (request.radius() != null) ? request.radius() : 20.0;
+			double radiusMeters = radiusKm * 1000;
+
+			return observationRepository.findSpotsWithinRadius(request.lat(), request.lon(), radiusMeters)
+				.stream()
+				.map(ObservationSpotResponse::from)
+				.toList();
+		}
 
 		return observationRepository.findAll()
-			.stream().map(ObservationSpotResponse::from)
+			.stream()
+			.map(ObservationSpotResponse::from)
 			.toList();
 	}
 }
