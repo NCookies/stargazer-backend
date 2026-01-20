6fa35247fe604256a6c0a1f4cd19ae090742d61a
SeungWoo Ryu <swstar21c@gmail.com>
2026-01-20 16:03:10 +0900

    feat: CookieUtil 구현 및 적용
    - refresh token을 HttpOnly 쿠키에 담거나 무효화 시키는 유틸 클래스 구현
    - 프론트엔드 리디렉션 주소를 환경변수에서 참조하도록 수정

--- src/main/java/xyz/ncookie/stargazer/global/security/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/xyz/ncookie/stargazer/global/security/handler/OAuth2LoginSuccessHandler.java
@@ -2,6 +2,7 @@ package xyz.ncookie.stargazer.global.security.handler;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
@@ -15,14 +16,20 @@ import lombok.extern.slf4j.Slf4j;
 import xyz.ncookie.stargazer.global.security.jwt.JwtTokenProvider;
 import xyz.ncookie.stargazer.global.security.principal.MemberPrincipal;
 import xyz.ncookie.stargazer.global.security.redis.RefreshTokenRedisRepository;
+import xyz.ncookie.stargazer.global.security.util.CookieUtil;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-	private final JwtTokenProvider jwtTokenProvider;
 	private final RefreshTokenRedisRepository refreshTokenRedisRepository;
+
+	private final JwtTokenProvider jwtTokenProvider;
+	private final CookieUtil cookieUtil;
+
+	@Value("${client.redirect-base-url}")
+	private String webClientBaseUrl;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
@@ -39,20 +46,15 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
 
 		refreshTokenRedisRepository.save(refreshToken, memberId, JwtTokenProvider.REFRESH_EXPIRE_MS);
 
-		ResponseCookie rtCookie = ResponseCookie.from("refreshToken", refreshToken)
-			.httpOnly(true)
-			.secure(true)
-			.sameSite("None")
-			.path("/")
-			.maxAge(JwtTokenProvider.REFRESH_EXPIRE_MS / 1000)
-			.build();
 
+		ResponseCookie rtCookie = cookieUtil.createRefreshTokenCookie(refreshToken);
 		response.addHeader(HttpHeaders.SET_COOKIE, rtCookie.toString());
 
-		response.sendRedirect("http://localhost:3000/oauth/callback");
-		// response.setContentType("application/json");
-		// response.getWriter().write(
-		// 	"{\"accessToken\":\"" + accessToken + "\"}"
-		// );
+		// 우선 React 전용
+		response.setContentType("application/json");
+		response.getWriter().write(
+			"{\"accessToken\":\"" + accessToken + "\"}"
+		);
+		response.sendRedirect(webClientBaseUrl + "/oauth/callback");
 	}
 }
