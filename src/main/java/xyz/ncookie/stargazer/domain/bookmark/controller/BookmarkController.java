7e43898c8c628cbad5d3a8dc94ce8a49291223df
SeungWoo Ryu <swstar21c@gmail.com>
2026-01-29 01:53:01 +0900

    refactor: 북마크 이름 수정 API PUT -> PATCH로 변경
    - 북마크 이름 하나만 바꾸는 API이므로 PATCH가 맥락 상 더 작합하다고 판단함

--- src/main/java/xyz/ncookie/stargazer/domain/bookmark/controller/BookmarkController.java
+++ src/main/java/xyz/ncookie/stargazer/domain/bookmark/controller/BookmarkController.java
@@ -6,9 +6,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -43,7 +43,7 @@ public class BookmarkController {
 		return bookmarkService.addBookmark(principal.getMemberId(), request);
 	}
 
-	@PutMapping("/{bookmarkId}")
+	@PatchMapping("/{bookmarkId}")
 	public BookmarkResponse modifyBookmark(
 		@AuthenticationPrincipal MemberPrincipal principal,
 		@PathVariable Long bookmarkId,
